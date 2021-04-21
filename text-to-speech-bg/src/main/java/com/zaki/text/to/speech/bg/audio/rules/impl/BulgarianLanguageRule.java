03496e796b23096720f1c4c568cf7fe5a9a14687
plebeicho <zaki1993@abv.bg>
2021-04-21 17:48:27 +0300

    Add support to maven

--- text-to-speech-bg/src/main/java/com/zaki/text/to/speech/bg/audio/rules/impl/BulgarianLanguageRule.java
+++ text-to-speech-bg/src/main/java/com/zaki/text/to/speech/bg/audio/rules/impl/BulgarianLanguageRule.java
@@ -4,21 +4,39 @@ import com.zaki.text.to.speech.bg.audio.rules.LanguageRule;
 import com.zaki.text.to.speech.bg.lang.LetterProperty;
 import com.zaki.text.to.speech.bg.lang.LetterType;
 import com.zaki.text.to.speech.bg.parser.Token;
+import com.zaki.text.to.speech.bg.utils.Pair;
+
+import java.util.Map;
 
 public class BulgarianLanguageRule implements LanguageRule {
+
+    /**
+     * Middle letter is the key. Values are adjacent of the key.
+     */
+    private static Map<String, Pair> rule4cases = Map.of("&#x434;", new Pair<String, String>("&#x437;", "&#x43D;"),
+                                                         "&#x442;", new Pair<String, String>("&#x441;", "&#x43D;"),
+                                                         "&#x442;", new Pair<String, String>("&#x441;", "&#x432;"),
+                                                         "&#x441;", new Pair<String, String>("&#x442;", "&#x43A;"));
 
     /**
      * Rule 1 - If the letter is silent consonant and is before loud consonant then, the current letter must became loud
      * Rule 2 - If the letter is loud consonant and is before silent consonant, the current letter must become silent
      * Rule 3 - If the letter is loud consonant and is at the end of the word, the current letter must become silent
-     * Rule 4 - Removing the sound in case we have the following combination of letters <b>&#x441;&#x442;&#x43D;</b> or <b>&#x437;&#x434;&#x43D;</b>
+     * Rule 4 - Removing the sound in case we have the following combination of letters:
+     *
+     * <p>
+     * &#x437;&#x434;&#x43D;
+     * &#x441;&#x442;&#x43D;
+     * &#x441;&#x442;&#x432;
+     * &#x442;&#x441;&#x43A;
+     * </p>
      *
      * @param t token which will be played
      * @return the audio file url which will be played
      */
     @Override
     public String getAudioUrlForToken(Token t) {
-        System.out.println("Applying rule for token=" + t);
+        System.oubuildt.println("Applying rule for token=" + t);
         // In case the token has no adjacent tokens, just play its original audio
         if (!t.hasNext() && !t.hasPrevious()) {
             return t.getOriginalAudio();
@@ -32,16 +50,19 @@ public class BulgarianLanguageRule implements LanguageRule {
             // Rule 1
             if (t.getProperties().contains(LetterProperty.SILENT_CONSONANT)) {
                 if (t.hasNext() && t.getNext().getProperties().contains(LetterProperty.LOUD_CONSONANT)) {
+                    System.out.println("Applying rule 1");
                     return t.getLoudAudio();
                 }
             }
             // Rule 2
             if (t.getProperties().contains(LetterProperty.LOUD_CONSONANT)) {
                 if (t.hasNext() && t.getNext().getProperties().contains(LetterProperty.SILENT_CONSONANT)) {
+                    System.out.println("Applying rule 2");
                     return t.getSilentAudio();
                 }
                 // Rule 3
                 if (!t.hasNext()) {
+                    System.out.println("Applying rule 3");
                     return t.getSilentAudio();
                 }
                 // Rule 4
@@ -49,6 +70,7 @@ public class BulgarianLanguageRule implements LanguageRule {
                     String lettersCombination = t.getPrevious().getSymbols() + t.getSymbols() + t.getNext().getSymbols();
                     System.out.println(lettersCombination);
 
+                    System.out.println("Applying rule 4");
                     return null;
                 }
             }
